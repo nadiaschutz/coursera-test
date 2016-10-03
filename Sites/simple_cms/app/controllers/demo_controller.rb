68154fa8413a166e8d5a92ce829aaed42487eb17
nadiaikon <nadiaikon@gmail.com>
2016-10-03 15:26:28 -0400

    adding staff

---
+++ Sites/simple_cms/app/controllers/demo_controller.rb
@@ -0,0 +1,16 @@
+class DemoController < ApplicationController
+	layout false
+  def index
+  	render('hello')
+  end
+  def hello
+  	#render('index')
+  	@array= [1,2,3,4,5]
+  end
+  def other_hello
+  	redirect_to(:contorller => 'demo', :action =>'index')
+  end
+  def lynda
+  	redirect_to("http://lynda.com")
+  end
+end
